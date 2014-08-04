546265d3fda94f2f3603a9f477008b20ac27c41f
David Gray <grayda@solidinc.org>
2014-08-04 23:49:27 +1000

    Big changes to the emulator
    Emulator now works with SmartPoint app (socket is added to the app but
    isn't controllable yet)

--- emulator.js
+++ emulator.js
@@ -23,42 +23,123 @@ var state = "01"; */
 
 function OrviboEmulator() {
 	EventEmitter.call(this); // Needed so we can emit() from this module
-
+	console.log(localIP);
 	scktServer.on('message', function (message, remote) { // We've got a message back from the network
 		if (remote.address != localIP) { //Check message isn't from us
 			var MessageHex = new Buffer(message).toString('hex'); // Convert our message into a string of hex
+			this.emit('data', message, remote);
 			var remoteMac = MessageHex.substr(MessageHex.indexOf('accf'), 12); // Look for the first occurance of ACCF (the start of our MAC address) and grab it, plus the next 12 bytes
 			index = hosts.map(function(e) { return e.macAddress; }).indexOf(remoteMac); // Use the arr.map() and indexOf functions to find out where in our array, our socket is
 			var type;
 				switch(MessageHex.substr(0,12)) { // Look for the first twelve bytes
 					case "686400067161":
+						console.log("Discovery request from: " + remote.address);
 						hosts.forEach(function(item) {
-							console.log("Discovery request from: " + remote.address);
+
+							item.remote = remote.address;
+							item.ready = true;
+						
 							// C0192423CFAC202020202020534F4330303228CA6CD701
-							payload = "6864002a716100" + item.macAddress + twenties + item.macReversed + twenties + "534F4330303228CA6CD7" + item.state;
+							payload = "6864002a716100" + item.macAddress + twenties + item.macReversed + twenties + "534F43303032FED989D7" + item.state;
 							this.sendMessage(hex2ba(payload),remote.address);
 							console.log("Discovery response sent");
 							
 						}.bind(this));
+						break;
+					case "686400127167":
+						mIndex = hosts.map(function(e) { return e.macAddress; }).indexOf(remoteMac);
+						if(mIndex > -1) {
+							hosts[index].remote = remote.address;
+							hosts[index].ready = true;
+							payload = "6864002a716100" + hosts[mIndex].macAddress + twenties + hosts[mIndex].macReversed + twenties + "534F43303032FED989D7" + hosts[mIndex].state;
+							this.sendMessage(hex2ba(payload),remote.address);						
+						}
 						break;
 					case "6864001e636c":
 						console.log("Subscription request from " + remote.address);
 						payload = "68640018636C" + hosts[index].macAddress + twenties + "0000000000" + hosts[index].state
 						this.sendMessage(hex2ba(payload),remote.address);
 						console.log("Subscription response sent");
-					case "6864001D7274":
+						break;
+					case "6864001d7274":
 						console.log("Query request from " + remote.address);
 						namepad = S(hosts[index].name).padRight(16, " ").s;
 						namepad = new Buffer(namepad);
-						payload = "686400A87274" + hosts[index].macAddress + twenties + "020000000004000100008A0001004325" + hosts[index].macAddress + twenties + hosts[index].macReversed + twenties + "383838383838" + twenties + namepad.toString('hex') + "0100" + "10000000090000000500000010272a796fd01027766963656e7465722e6f727669626f2e636f6d202020202020202020202020202020202020202020c0a801c8c0a80101ffffff000101000a00ff0000";
+						ip = "C0A8010E" // REPLACE ME!
+						
+						switch(MessageHex.substr(MessageHex.length - 14, 2)) {
+							case "01":
+								console.log("Table 1");
+								payload = "686400247274"
+									+ hosts[index].macAddress
+									+ twenties
+									+ "020000000001000100000600040004000200";
+								break;
+							case "04":
+								console.log("Table 4");							
+								payload = "686400A87274" // Magic key, message length, command ID
+									+ hosts[index].macAddress 
+									+ twenties 
+									+ "020000000004000100008A0001004325" // Record number and other junk we don't care about :)
+									+ hosts[index].macAddress 
+									+ twenties 
+									+ hosts[index].macReversed 
+									+ twenties 
+									+ "383838383838" // Remote password
+									+ twenties // Padding for the remote password
+									+ namepad.toString('hex') // Socket name including padding
+									+ "0400" // The socket icon
+									+ "10000000" // Hardware version
+									+ "09000000" // Firmware version
+									+ "05000000" // Chip firmware version
+									+ "1027" // Port 10000
+									+ "2a796fd0" // The remote port (for remote access?)
+									+ "1027" // Remote port 10000
+									+ "766963656e7465722e6f727669626f2e636f6d" // vicenter.orvibo.com
+									+ "202020202020202020202020202020202020202020" // And padding for remote address above
+									+ ip
+									+ "c0a80101" // Local gateway (modem etc.)
+									+ "ffffff00" // Subnet mask
+									+ "01" // DHCP is on?
+									+ "01" // Discoverable?
+									+ "000a" // Timezone
+									+ "0000" // ?? Sample data says 0000 but real data says 00FF. What means?
+									+ "0000"; // Countdown timer
+								break;
+							default:
+								throw "Unhandled table data. Hex was: " + MessageHex.toString('hex');
+								break;
+						}
+						
+						
 						this.sendMessage(hex2ba(payload),remote.address);
 						console.log("Query reply sent");
+						break;
+					case "6864001a6373": // I don't know what this is, but the SmartPoint app asks the socket for it, so here it is!
+						console.log("Data Packet 'A' received");
+						payload = "686400176373"
+							+ hosts[index].macAddress
+							+ twenties
+							+ "0000000000"; // Does state go on the end here?
+						this.sendMessage(hex2ba(payload),remote.address);
+						console.log("Data Packet 'A' responded to");						
+						break;
+					case "686400166862":
+						console.log("Data Packet 'B' received");					
+						payload = "686400176862"
+							+ hosts[index].macAddress
+							+ twenties
+							+ "0000000000";
+						this.sendMessage(hex2ba(payload),remote.address);
+						console.log("Data Packet 'A' responded to");
+						break;							
 					case "686400176463":
 						console.log("Request to change state received");
 						hosts[index].state = MessageHex.substr(MessageHex.length - 2,2) == "01" ? "01" : "00";
 						payload = "686400177366" + hosts[index].macAddress + "00000000" + hosts[index].state;
 						this.sendMessage(hex2ba(payload),remote.address);
 						console.log("Response to state change sent. New state is: " + hosts[index].state);
+						break;
 		
 				}
 			
@@ -96,7 +177,13 @@ OrviboEmulator.prototype.setName = function(index, sName) {
 }
 
 OrviboEmulator.prototype.setState = function(index, sState) {
-	hosts[index].state = sState	
+	if(hosts[index].ready == true) {
+		console.log("State: " + sState);
+		hosts[index].state = sState
+		console.log("State: " + sState);
+		payload = "686400177366" + hosts[index].macAddress + "00000000" + hosts[index].state;
+		this.sendMessage(hex2ba(payload),hosts[index].remote);
+	}
 }
 
 OrviboEmulator.prototype.getMACAddress = function(index) {
@@ -110,6 +197,7 @@ OrviboEmulator.prototype.sendMessage = function(message, sHost, callback) {
     process.nextTick(function() { // Next time we're processing stuff. To keep our app from running away from us, I suppose
 		scktClient.send(message, 0, message.length, port, sHost, function(err, bytes) { // Send the message. Parameter 2 is offset, so it's 0. 
 	        if (err) throw err; // Error? CRASH AND BURN BB!
+			this.emit('sent', message, sHost);
 	    }.bind(this)); // Again, we do .bind(this) so calling this.emit(); comes from OrviboSocket, and not from scktClient
 		if(typeof callback === "function") { callback(); } // And if we've specified a callback function, go right ahead and do that, as we've sent the message
 	}.bind(this));
@@ -124,12 +212,12 @@ function getBroadcastAddress() { // A bit of code that lets us get our network I
 	    for (k2 in interfaces[k]) { // And our sub-interfaces
 	        var address = interfaces[k][k2]; // Get the address 
 	        if (address.family == 'IPv4' && !address.internal) { // If we're IPv4 and it's not an internal address (like 127.0.0.1)
-	            addresses.push(address.address) // Shove it onto our addresses array
+	            return address.address // Shove it onto our addresses array
 	        }
 	    }
 	}
 
-	return addresses;
+	return 0;
 }
 
 function hex2ba(hex) { // Takes a string of hex and turns it into a byte array: ['0xAC', '0xCF] etc.
@@ -146,6 +234,15 @@ function hex2a(hexx) { // Takes a hex string and turns it into an ASCII string
     for (var i = 0; i < hex.length; i += 2)
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
+}
+
+OrviboEmulator.prototype.intToHex = function(int) {
+	return int.toString(16)
+}
+
+OrviboEmulator.prototype.strToHex = function (str) {
+	str = new Buffer(str);
+	return str.toString('hex');
 }
 
 module.exports = OrviboEmulator; // And make every OrviboSocket function available to whatever file wishes to use it. 
--- tests/emulatorTest.js
+++ tests/emulatorTest.js
@@ -1,9 +1,38 @@
 var OrviboEmulator = require("../emulator");
 var o = new OrviboEmulator();
 
-o.hosts.push({ index: 0, macAddress: "accfabcdef12", macReversed: "12efcdabcfac", name: "Test Socket 1", state: "01" });
-o.hosts.push({ index: 1, macAddress: "accfdeadbeef", macReversed: "efbeaddecfac", name: "Dead Beef", state: "00"  });
-o.hosts.push({ index: 2, macAddress: "accf12345678", macReversed: "78563412cfac", name: "Numbers Station", state: "01"  });
-o.hosts.push({ index: 2, macAddress: "accffadedcab", macReversed: "abdcdefacfac", name: "Faded Cab", state: "00"  });
+/* To add new sockets, copy and paste this line and change as necessary */
+o.hosts.push({ index: 1, remote: '', ready: false, macAddress: "accfdeadbeef", macReversed: "efbeaddecfac", name: "Dead Beef", state: "00"  });
+
+var readline = require('readline'),
+rl = readline.createInterface(process.stdin, process.stdout);
+
+console.log("List of sockets to be created:");
 console.dir(o.hosts);
+
 o.prepare();
+count = o.hosts.length - 1;
+rl.setPrompt('Enter an index to toggle (0 to ' + count.toString() + ")");
+rl.prompt();
+rl.on('line', function(line) {
+	try {
+		if(line == "status") { 
+			console.log("State of sockets:");
+			console.dir(o.hosts); 
+		} else {
+			o.setState(parseInt(line), o.hosts[parseInt(line)].state == "00" ? "01" : "00");
+		}
+	} catch(ex) {
+		console.log("Error setting state. Error was: " + ex);	
+	}
+	
+	rl.prompt();
+  });
+  
+o.on('data', function(data, ip) {
+	console.log("Data received: " + data.toString('hex') + " from " + ip.address); 
+});
+  
+o.on('sent', function(data, ip) {
+	console.log("Data SENT: " + data.toString('hex') + " to " + ip); 
+});
